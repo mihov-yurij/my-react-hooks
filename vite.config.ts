3bdd4f4eaef1b262c798b4b1ef95d5c22eea99de
yurij <mihov.yurij@gmail.com>
2026-04-02 04:08:10 +0300

    update project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@vitejs/plugin-react' // 1. Импортируй это
+
 
 export default defineConfig({
   plugins: [
